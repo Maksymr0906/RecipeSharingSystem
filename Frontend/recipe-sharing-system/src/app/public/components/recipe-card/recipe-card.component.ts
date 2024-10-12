f6d447affe01b0e25c493b6eb99a99413f3be4ca
maksymr0906 <maksymr0906@gmail.com>
2024-10-12 11:58:45 +0300

    implement recipe card

--- Frontend/recipe-sharing-system/src/app/public/components/recipe-card/recipe-card.component.ts
+++ Frontend/recipe-sharing-system/src/app/public/components/recipe-card/recipe-card.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/features/category/models/category.model';
+import { CategoryService } from 'src/app/features/category/services/category.service';
+import { Recipe } from 'src/app/features/recipe/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.css']
 })
-export class RecipeCardComponent {
+export class RecipeCardComponent implements OnInit {
+  @Input() recipe: Recipe = {
+      id: '',
+      title: '',
+      shortDescription: '',
+      featuredImageUrl: '',
+      instructionId: '',
+      ratingIds: [],
+      categoryIds: [],
+      ingredients: []
+  };
+  
+  categories: Category[] = []
 
+  constructor(private categoryService: CategoryService) {
+  }
+
+  ngOnInit(): void {
+    this.recipe.categoryIds.forEach(id => {
+      this.categoryService.getCategoryById(id).subscribe(category => {
+        this.categories.push(category);
+      })
+    })
+  }
 }
